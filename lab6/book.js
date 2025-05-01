74cea69fdcb92096e2b1795b6c7755d0eef3dcdb
Vika Fot <fot.vicctoria@gmail.com>
2025-05-01 22:51:30 +0700

    Книги

---
+++ lab6/book.js
@@ -0,0 +1,82 @@
+const books = [ 
+    {
+        author: 'Анна Джейн',
+        name: 'Тайный сад желаний',
+        publicationYear: 2023,
+        price: 560,
+        bestseller: true,
+        inStock: true
+    },
+    {
+        author: 'Анна Джейн',
+        name: 'Мой идеальный бывший',
+        publicationYear: 2020,
+        price: 390,
+        bestseller: true,
+        inStock: true
+    },
+    {
+        author: 'Анна Джейн',
+        name: 'Не отпускай меня',
+        publicationYear: 2022,
+        price: 450,
+        bestseller: true,
+        inStock: true
+    },
+    {
+        author: 'Анна Джейн',
+        name: 'Три желания',
+        publicationYear: 2021,
+        price: 420,
+        bestseller: false,
+        inStock: false
+    },
+    {
+        author: 'Анна Джейн',
+        name: 'Мир, в котором я исчезаю',
+        publicationYear: 2023,
+        price: 470,
+        bestseller: true,
+        inStock: true
+    },
+    {
+        author: 'Анна Джейн',
+        name: 'Игра с огнём',
+        publicationYear: 2021,
+        price: 410,
+        bestseller: false,
+        inStock: true
+    },
+    {
+        author: 'Анна Джейн',
+        name: 'Без права на ошибку',
+        publicationYear: 2020,
+        price: 430,
+        bestseller: false,
+        inStock: true
+    },
+    {
+        author: 'Анна Джейн',
+        name: 'Ты — мой лучший враг',
+        publicationYear: 2019,
+        price: 370,
+        bestseller: true,
+        inStock: false
+    },
+    {
+        author: 'Анна Джейн',
+        name: 'Линия света',
+        publicationYear: 2018,
+        price: 390,
+        bestseller: false,
+        inStock: true
+    },
+    {
+        author: 'Анна Джейн',
+        name: 'Игра теней',
+        publicationYear: 2023,
+        price: 480,
+        bestseller: true,
+        inStock: true
+    }
+];
